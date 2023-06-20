e53c260fc3ad39cdafb2179a18b6e899dfc349f4
aaran <nabeel.n@blockapex.io>
2023-06-20 14:01:55 +0500

    improve:removed redundant comments

--- src/utils/pricing.ts
+++ src/utils/pricing.ts
@@ -18,18 +18,7 @@ export let WHITELIST_TOKENS: string[] = [
   '<ARB>', // ARB
   '<OP>', // OP
   '<UNI>', // UNI
-  // '0x57ab1ec28d129707052df4df418d58a2d46d5f51', // sUSD
-  // '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2', // MKR
-  // '0xc00e94cb662c3520282e6f5717214004a7f26888', // COMP
-  // '0x514910771af9ca656af840dff83e8264ecf986ca', // LINK
-  // '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f', // SNX
-  // '0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e', // YFI
-  // '0x111111111117dc0aa78b770fa6a738034120c302', // 1INCH
-  // '0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8', // yCurv
-  // '0x956f47f50a910163d8bf957cf5846d573e7f87ca', // FEI
   '<MATIC>' // MATIC
-  // '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9', // AAVE
-  // '0xfe2e637202056d30016725477c5da089ab0a043a' // sETH2
 ]
 let usdc_address = ''
 
